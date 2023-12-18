b49af7a5f573beed36a7d4c6b0410a9077a7020b
zjk <2455650395@qq.com>
2023-12-18 18:12:15 +0800

    modify annotation

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling     //开启定时器
 @ServletComponentScan //该注解可以自动注入@WebServlet，@WebFilter，@WebListener。否则在过滤器中无法获取到自动注入得bean
 @EnableTransactionManagement // 启注解事务管理，
-@MapperScan(basePackages = {"com.compant.project.dao.*.*"}) //解决多个dao层无法注入bean的问题
+@MapperScan(basePackages = {"com.company.project.dao.*.*"}) //解决多个dao层无法注入bean的问题
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
